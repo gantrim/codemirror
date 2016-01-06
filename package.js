57103723b83abacfa52b0f6cd2f070601f6e6ce4
Greg Antrim <g.antrim@kunzleigh.com>
2016-01-06 13:59:19 -0500

    added modes (code types back)

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
     name: 'gantrim:codemirror',
     summary: 'CodeMirror editor for Meteor >= 1.2',
-    version: '1.3.1',
+    version: '1.3.22',
     git: 'https://github.com/gantrim/codemirror.git'
 });
 
 Package.onUse(function (api) {
 
-    if(api.versionsFrom) {
-		api.versionsFrom('METEOR@1.2');
-	}
-	
-     api.use([
+    if (api.versionsFrom) {
+        api.versionsFrom('METEOR@1.2');
+    }
+
+    api.use([
         'templating@1.1.5',
         'underscore@1.0.4'
     ], 'client');
@@ -165,12 +165,12 @@ Package.onUse(function (api) {
     //TODO: Fix merge.js diff_match_patch
     //
     ///**
-    // *	Extras: merge
+    // *    Extras: merge
     // */
-    //[	'merge.js',
-    //	'merge.css'
+    //[ 'merge.js',
+    //  'merge.css'
     //].forEach(function(item) {
-    //	api.addFiles('lib/codemirror/addon/merge/' + item, 'client');
+    //  api.addFiles('lib/codemirror/addon/merge/' + item, 'client');
     //});
 
 
@@ -215,6 +215,169 @@ Package.onUse(function (api) {
         api.addFiles('lib/codemirror/addon/wrap/' + item, 'client');
     });
 
-    // api.export('CodeMirror', 'client');
+    var themeCSSFiles = [
+        "3024-day.css",
+        "3024-night.css",
+        "abcdef.css",
+        "ambiance-mobile.css",
+        "ambiance.css",
+        "base16-dark.css",
+        "base16-light.css",
+        "bespin.css",
+        "blackboard.css",
+        "cobalt.css",
+        "colorforth.css",
+        "dracula.css",
+        "eclipse.css",
+        "elegant.css",
+        "erlang-dark.css",
+        "hopscotch.css",
+        "icecoder.css",
+        "isotope.css",
+        "lesser-dark.css",
+        "liquibyte.css",
+        "material.css",
+        "mbo.css",
+        "mdn-like.css",
+        "midnight.css",
+        "monokai.css",
+        "neat.css",
+        "neo.css",
+        "night.css",
+        "paraiso-dark.css",
+        "paraiso-light.css",
+        "pastel-on-dark.css",
+        "railscasts.css",
+        "rubyblue.css",
+        "seti.css",
+        "solarized.css",
+        "the-matrix.css",
+        "tomorrow-night-bright.css",
+        "tomorrow-night-eighties.css",
+        "ttcn.css",
+        "twilight.css",
+        "vibrant-ink.css",
+        "xq-dark.css",
+        "xq-light.css",
+        "yeti.css",
+        "zenburn.css"
+    ];
+    themeCSSFiles.forEach(function (cssFile) {
+        api.add_files('lib/codemirror/theme/' + cssFile, "client");
+    });
 
+    var modes = [
+        "apl",
+        "asciiarmor",
+        "asn.1",
+        "asterisk",
+        "brainfuck",
+        "clike",
+        "clojure",
+        "cmake",
+        "cobol",
+        "coffeescript",
+        "commonlisp",
+        "css",
+        "cypher",
+        "d",
+        "dart",
+        "diff",
+        "django",
+        "dockerfile",
+        "dtd",
+        "dylan",
+        "ebnf",
+        "ecl",
+        "eiffel",
+        "elm",
+        "erlang",
+        "factor",
+        "forth",
+        "fortran",
+        "gas",
+        "gfm",
+        "gherkin",
+        "go",
+        "groovy",
+        "haml",
+        "handlebars",
+        "haskell",
+        "haxe",
+        "htmlembedded",
+        "htmlmixed",
+        "http",
+        "idl",
+        "jade",
+        "javascript",
+        "jinja2",
+        "julia",
+        "livescript",
+        "lua",
+        "markdown",
+        "mathematica",
+        "mirc",
+        "mllike",
+        "modelica",
+        "mscgen",
+        "mumps",
+        "nginx",
+        "nsis",
+        "ntriples",
+        "octave",
+        "oz",
+        "pascal",
+        "pegjs",
+        "perl",
+        "php",
+        "pig",
+        "properties",
+        "puppet",
+        "python",
+        "q",
+        "r",
+        "rpm",
+        "rst",
+        "ruby",
+        "rust",
+        "sass",
+        "scheme",
+        "shell",
+        "sieve",
+        "slim",
+        "smalltalk",
+        "smarty",
+        "solr",
+        "soy",
+        "sparql",
+        "spreadsheet",
+        "sql",
+        "stex",
+        "stylus",
+        "swift",
+        "tcl",
+        "textile",
+        "tiddlywiki",
+        "tiki",
+        "toml",
+        "tornado",
+        "troff",
+        "ttcn-cfg",
+        "ttcn",
+        "turtle",
+        "twig",
+        "vb",
+        "vbscript",
+        "velocity",
+        "verilog",
+        "vhdl",
+        "vue",
+        "xml",
+        "xquery",
+        "yaml",
+        "z80"
+    ];
+    modes.forEach(function (mode) {
+        api.add_files('lib/codemirror/mode/' + mode + "/" + mode + ".js", "client");
+    });
 });
